751b4f15be83cdd332a0d6ad88d0ca7cf288c076
LukasA <lukas.astrauskas@gmail.com>
2022-04-14 12:21:24 +0300

    moking conflict

--- src/main/java/hashpass/HashingPassword.java
+++ src/main/java/hashpass/HashingPassword.java
@@ -31,7 +31,7 @@ public class HashingPassword {
         boolean isTrue = true;
         int numberToIncrease =0;
         while (numberToIncrease<10) {
-            System.out.println("Demo branch");
+            System.out.println("Main branch");
             isTrue = !isTrue;
             numberToIncrease++;
         }
